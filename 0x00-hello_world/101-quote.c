91a9c99d7cfb167016bbc139e9571e79dc03e02e
nobleemeka <nobleemeka9@gmail.com>
2022-09-12 19:40:34 -0700

    my last task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
 /**
- * main - Entry point
- * Return:1
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * follwed by a new line to standard error
+ *
+ * Return: Always 1
  */
 int main(void)
 {
-	char ch[] = "and that piece of art is useful \" - Dora Korpar, 2015-10-19\n";
-	write(2, ch, sizeof(ch));
+	write(2,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
