b3d28861f279014024ee1066ad661bfba7c81df3
nobleemeka <nobleemeka9@gmail.com>
2022-10-13 18:58:37 -0700

    function_pointers.h

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,18 @@
+#ifndef _function_pointers_h_
+#define _function_pointers_h_
+
+/*
+ * Structs, enums and unions definitions
+ * Typedefs
+ * Function prototypes
+ */
+#include <string.h>
+int _putchar(char);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int(*cmp)(int));
+
+
+
+
+#endif /*_function_pointers_h_*/
