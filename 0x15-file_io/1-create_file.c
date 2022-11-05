55938afdc452a963ce04a2410d145d8df454ed25
nobleemeka <nobleemeka9@gmail.com>
2022-11-04 20:20:29 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,7 @@
 /**
  * create_file - create a file
  * @filename: the name of the file to create
- * @text_content: a NULL terminate string to write to the file
+ * text_content: a NULL terminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
@@ -23,9 +23,7 @@ int create_file(const char *filename, char *text_content)
 		fwrite = write(file, text_content, i);
 		if (fwrite == -1)
 			return (-1);
-		return (-1);
 	}
 	close(file);
 	return (1);
 }
-
